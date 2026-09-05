bb5f6860e2a319953ea84ebe3f3145dd7b0445c6
yusufi1010 <noviatiyusufi@gmail.com>
2026-09-05 13:36:07 +0700

    feat: menambahkan script utama aplikasi

---
+++ main.py
@@ -0,0 +1,6 @@
+# main.py
+def main():
+    print("=== SIKLUS PERANGKAT LUNAK SSD1030 ===")
+    print("Status: Environment VS Code & Git Berhasil Dikonfigurasi.")
+    if __name__ == "__main__":
+        main()
